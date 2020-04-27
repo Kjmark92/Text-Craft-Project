a561fc241dc8830a1c5d812e7fd75408d04ed9c9
rsm-gnadjaou <gnadjaou@ucsd.edu>
2020-04-27 00:06:09 +0000

    save

--- App/app.R
+++ App/app.R
@@ -285,11 +285,8 @@ server <- function(input, output){
              paste("<p>","Ellen P Walsh","<br>"),
              paste("ellen.walsh@rady.ucsd.edu"),
              
-             paste("<br>"),
-             paste("<a href =","https://www.ghislene.com/",">","Ghislene Adjaoute","</a>","<br>"),
-             paste("ghislene.adjaoute@rady.ucsd.edu"),
-             
-             paste("<p>","Ghislene Adjaoute","<br>"),
+             paste("<p>"),
+             paste("<a href =","https://www.ghislene.com/",">","Ghislene Adjaoute, MSW","</a>","<br>"),
              paste("ghislene.adjaoute@rady.ucsd.edu")
              
              
